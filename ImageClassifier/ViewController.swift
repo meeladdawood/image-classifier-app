442314023167ac094387183c351108216b18065b
Meelad Dawood <mdawood@sandiego.edu>
2018-01-14 14:07:10 -0800

    Implemented the cameraButtonPressed func to take an image using the imagePicker variable which is of UIImagePickerController and then display that image in the imageView. Also modified the .plist to gain access for camera usage and photo library access.

--- ImageClassifier/ViewController.swift
+++ ImageClassifier/ViewController.swift
@@ -20,10 +20,27 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        imagePicker.delegate = self
+        imagePicker.sourceType = .camera
+        imagePicker.allowsEditing = false
+        
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            imageView.image = userPickedImage
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
+        
     }
 
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
+        present(imagePicker, animated: true, completion: nil)
         
     }
     
