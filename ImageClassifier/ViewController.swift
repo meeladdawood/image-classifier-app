a7f73333468f599a7f14a6b4171b9408f438d943
Meelad Dawood <mdawood@sandiego.edu>
2018-01-14 14:28:38 -0800

    Added a detect func that sends a request to the Inception V3 ML model.

--- ImageClassifier/ViewController.swift
+++ ImageClassifier/ViewController.swift
@@ -32,12 +32,40 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image : userPickedImage) else {
+                fatalError("Could not convert to CIImage")
+            }
+            detect(image: ciImage)
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
         
     }
 
+    func detect(image: CIImage ){
+        
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading CoreML model failed.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            guard let results =  request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch{
+            print(error)
+        }
+        
+    }
+    
+    
     @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
