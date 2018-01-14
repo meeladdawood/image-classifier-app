fb0f45e380ffa90ee1fec2e389c6bef5d6339bff
Meelad Dawood <mdawood@sandiego.edu>
2018-01-14 14:48:45 -0800

    Added a classification statements that classifies whether an image is a "Hot Dog" or "Not a hot god" very similar to the Silicon Valley application.

--- ImageClassifier/ViewController.swift
+++ ImageClassifier/ViewController.swift
@@ -53,7 +53,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
             guard let results =  request.results as? [VNClassificationObservation] else {
                 fatalError("Model failed to process image")
             }
-            print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hot dog"){
+                    self.navigationItem.title = "Hot Dog!"
+                } else {
+                    self.navigationItem.title = "Not Hot Dog"
+                }
+            }
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
