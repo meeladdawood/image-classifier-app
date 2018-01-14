12d10942ccaa765d5274c32ba570fe6c850965a5
Meelad Dawood <mdawood@sandiego.edu>
2018-01-14 13:56:47 -0800

    Added the inceptionV3 machine learning model, modified the storyboard by adding a navigation controller and adding a camera bar button. Linked camera button with func cameraButtonPressed and linked the image view with the image view variable.

--- ImageClassifier/ViewController.swift
+++ ImageClassifier/ViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet var imageView: UIImageView!
+    
+    let imagePicker = UIImagePickerController()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        
+        
     }
+    
 
 
 }
